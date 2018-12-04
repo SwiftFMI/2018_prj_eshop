16d68dadf862f996870678fa242772d773997aa8
grade <grade@lab122-08.local>
2018-12-04 15:25:21 +0200

    Init commit

---
+++ E Shop/E Shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  E Shop
+//
+//  Created by grade on 4.12.18.
+//  Copyright © 2018 teameshop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

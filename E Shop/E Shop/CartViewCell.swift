b8666a66edb22391633ef38e6e034cfdb5d7c964
Julian Stoev <stoefff@Julians-Mac.local>
2019-02-20 01:20:24 +0200

    Prototype of Cart Item as Collection View Cell

---
+++ E Shop/E Shop/CartViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CartViewCell.swift
+//  E Shop
+//
+//  Created by Julian Stoev on 19.02.19.
+//  Copyright © 2019 teameshop. All rights reserved.
+//
+
+import UIKit
+
+class CartViewCell: UICollectionViewCell {
+    
+    //static let height = CGFloat(80)
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
